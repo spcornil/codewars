18520dbf912e8a107b4b5b3f0bcb89ee2e968410
Sean Cornillie <sean.cornillie@gmail.com>
2023-11-02 13:33:21 -0600

    afternoon exercises

---
+++ 8_square_sum.py
@@ -0,0 +1,7 @@
+# Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+def square_sum(numbers):
+    sum = 0
+    for n in numbers:
+        sum += n**2
+    return sum
